91c117d1d1fd43cd512fbf58df5f5209cdc4baab
Doug Hammond <d.lakehammond@gmail.com>
2022-12-02 15:47:59 +0100

    Code reuse

---
+++ src/components/source-loader.js
@@ -0,0 +1,36 @@
+import { useState, useMemo } from 'react';
+import useFetch from 'use-http';
+
+import Stack from 'react-bootstrap/Stack';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import InputDisplay from 'components/input-display';
+import InputSelector from 'components/input-selector';
+
+const SourceLoader = ({ input, sample, parser, children }) => {
+  const [source, setSource] = useState(sample);
+  const request = useFetch(source, [source]);
+
+  const data = useMemo(() => (request.data && parser(request.data)), [request.data]);
+
+  return <Stack gap={3}>
+    <Stack gap={3} direction="horizontal" className="mx-auto">
+      <InputSelector value={sample} {...{source, setSource}}>Use sample</InputSelector>
+      <InputSelector value={input} {...{source, setSource}}>Use challenge</InputSelector>
+    </Stack>
+    <Container>
+      <Row>
+        <Col md="9">
+          {data && children(data)}
+        </Col>
+        <Col md="3">
+          <InputDisplay {...request} />
+        </Col>
+      </Row>
+    </Container>
+  </Stack>;
+};
+
+export default SourceLoader;
--- src/days/1.js
+++ src/days/1.js
@@ -1,13 +1,8 @@
-import { useState, useMemo } from 'react';
-import useFetch from 'use-http';
+import { useMemo } from 'react';
 
 import Stack from 'react-bootstrap/Stack';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
-import InputDisplay from 'components/input-display';
-import InputSelector from 'components/input-selector';
+import SourceLoader from 'components/source-loader';
 
 import input from '../input/1.txt';
 import sample from '../input/1.sample.txt';
@@ -29,39 +24,23 @@ const PartTwo = ({ data }) => {
   return <div>Part 2: {total}</div>;
 };
 
-const One = () => {
-  const [source, setSource] = useState(sample);
-  const request = useFetch(source, [source]);
+const parser = (data) => (
+  data.split('\n\n').map(elf =>
+    elf.split('\n').map(meal => Number.parseInt(meal))
+  )
+);
 
-  const data = useMemo(() => {
-    if (!request.data) return;
-
-    return request.data.split('\n\n').map(elf =>
-      elf.split('\n').map(meal => Number.parseInt(meal))
-    );
-  }, [request.data]);
-
-  return <Stack gap={3}>
-    <Stack gap={3} direction="horizontal" className="mx-auto">
-      <InputSelector value={sample} {...{source, setSource}}>Use sample</InputSelector>
-      <InputSelector value={input} {...{source, setSource}}>Use challenge</InputSelector>
+const One = () => (
+  <SourceLoader {...{input, sample, parser}}>{(data) => (
+    <Stack gap={3}>
+      <pre>{JSON.stringify(data)}</pre>
+      <View data={data} />
+      <Stack gap={3} direction="horizontal" className="mx-auto">
+        <PartOne data={data} />
+        <PartTwo data={data} />
+      </Stack>
     </Stack>
-    <Container>
-      <Row>
-        <Col md="9">
-          {data && <Stack gap={3}>
-            <pre>{JSON.stringify(data)}</pre>
-            <View data={data} />
-            <PartOne data={data} />
-            <PartTwo data={data} />
-          </Stack>}
-        </Col>
-        <Col md="3">
-          <InputDisplay {...request} />
-        </Col>
-      </Row>
-    </Container>
-  </Stack>;
-};
+  )}</SourceLoader>
+);
 
 export default One;
