7681f22071310e06659110b0387dfb9b16814973
Doug Hammond <d.lakehammond@gmail.com>
2022-12-12 20:50:49 +0100

    Efficiency

--- src/days/12/part-one.js
+++ src/days/12/part-one.js
@@ -9,20 +9,20 @@ const outOfBounds = (grid, { row, col }) => (
   row < 0 || col < 0 || row >= grid.length || col >= grid[row].length
 );
 
-const visit = (grid, pos, visited, toVisit) => {
-  if (visited[pos.row][pos.col]) return;
-  visited[pos.row][pos.col] = true;
+const visit = (grid, pos, touched, toVisit) => {
+  touched[pos.row][pos.col] = true;
 
   const height = grid[pos.row][pos.col];
 
   for (const neighbour of adjacency(pos)) {
     if (outOfBounds(grid, neighbour)) continue;
 
-    if (visited[neighbour.row][neighbour.col]) continue;
+    if (touched[neighbour.row][neighbour.col]) continue;
 
     if (grid[neighbour.row][neighbour.col] > height + 1) continue;
 
     toVisit.push({ ...neighbour, prev: pos });
+    touched[neighbour.row][neighbour.col] = true;
   }
 };
 
@@ -34,14 +34,14 @@ const pathLength = (pos) => {
 
 export const search = (grid, start, isEnd) => {
   const toVisit = [start];
-  const visited = [...Array(grid.length)].map(() => new Array(grid[0].length).fill(false));
+  const touched = [...Array(grid.length)].map(() => new Array(grid[0].length).fill(false));
 
   while (toVisit.length) {
     const next = toVisit.shift();
     if (isEnd(next))
       return pathLength(next);
 
-    visit(grid, next, visited, toVisit);
+    visit(grid, next, touched, toVisit);
   }
 };
 
